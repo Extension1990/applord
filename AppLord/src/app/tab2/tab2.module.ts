80fed11aa7bc69f0a22d2dcc064f49f799186a90
Extension1990 <extensiontlhareseng@gmail.com>
2021-02-25 18:21:11 +0200

    My first commit

--- AppLord/src/app/tab2/tab2.module.ts
+++ AppLord/src/app/tab2/tab2.module.ts
@@ -7,6 +7,7 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { ModalComponent } from '../modal/modal.component';
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     ExploreContainerComponentModule,
     Tab2PageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page, ModalComponent],
+  entryComponents: [ModalComponent]
 })
 export class Tab2PageModule {}
