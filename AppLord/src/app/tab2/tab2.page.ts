84e511a3f3bd3a83af19c46f9fb4d899e8eaee3f
Extension1990 <extensiontlhareseng@gmail.com>
2021-02-25 18:05:14 +0200

    My last commit

--- AppLord/src/app/tab2/tab2.page.ts
+++ AppLord/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from '../modal/modal.component';
 import { MainServiceService } from '../services/main-service.service';
 
 @Component({
@@ -10,7 +12,7 @@ export class Tab2Page implements OnInit {
 
   chars: any;
 
-  constructor(private service: MainServiceService) {}
+  constructor(private service: MainServiceService, private modalCltr: ModalController) {}
 
   ngOnInit() {
     this.getCharacters();
@@ -20,6 +22,14 @@ export class Tab2Page implements OnInit {
     this.service.getCharacters().subscribe((chars) => {
       this.chars = chars.results;
     });
+  }
+
+  async openModal() {
+    const modal = await this.modalCltr.create({
+      component: ModalComponent,
+    });
+
+    modal.present();
   }
 
 }
